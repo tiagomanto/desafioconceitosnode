d16613a853f3db212af617915992bcd8585796fe
Tiago Alexandre Mantovani <tiagoservodejesus@hotmail.com>
2020-09-04 10:31:10 -0300

    App

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { uuid, isUuid } =require('uuidv4');
+const { response } = require("express");
+//const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -9,25 +10,104 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+//verifica se é um id valido
+function validateProjectId(request, response, next){
+  const { id } = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({ error: 'Invalid project ID.'})
+  }
+  return next();
+}
+
+function logRequests(request, response, next){
+ const { method, url } = request
+ const logLabel = `[${method.toUpperCase()}] ${url}`;
+ console.time(logLabel) //mede o tempo de um console ate o outro
+ next();//proximo middleware
+
+ console.timeEnd(logLabel)// a mesma string nos parenteses
+
+}
+
+app.use(logRequests);
+app.use('/repositories/:id', validateProjectId) //valida somente nas rotas :id
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const repositorio = {
+    id: uuid(), 
+    title, 
+    url, 
+    techs, 
+    likes:0,
+  }
+  
+  repositories.push(repositorio)
+  return response.json(repositorio);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+
+  const findRepositoryIndex = repositories.findIndex(repositorio =>
+    repositorio === id
+  );  
+    //quando não existir
+  if (findRepositoryIndex===-1){
+    return response.status(400).json({error: 'Repositorio does not exists.'});
+  }
+
+  const repositorio ={
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[findRepositoryIndex].likes,
+  };
+
+repositories[findRepositoryIndex]=repositorio
+
+return response.json(repositorio);
+})
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const findRepositoryIndex = repositories.findIndex(repositorio =>
+    repositorio === id
+);
+  if (findRepositoryIndex >=0){
+    repositories.splice(findRepositoryIndex,1)
+  } else {
+    return response.status(400).json({ error: 'Repository does not exist'})
+  }
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  //busca o id da requisição
+  const { id } =request.params;
+
+  //busca o repositorio
+  const findRepositoryIndex= repositories.findIndex(repositorio =>
+    repositorio.id ===id 
+  );
+  //se não existir
+  if (findRepositoryIndex === -1){
+    return response.status(400).json({ error:'Repositorio does not exist.'});
+  }
+
+  //se existir
+  repositories[findRepositoryIndex].likes+=1;
+
+  //retorna o repositorio atualizado
+  return response.json(repositories[findRepositoryIndex]);
+
 });
 
 module.exports = app;
